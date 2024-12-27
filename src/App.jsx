2278e5fc2482050649ee5cf9b00688afc95e97cd
eremhia <eremhia.tech@gmail.com>
2024-12-27 21:14:21 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Client } from 'boardgame.io/react';
+import { TicTacToe } from './Game';
+import { TicTacToeBoard } from './Board';
+
+const App = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+});
+
+export default App;
